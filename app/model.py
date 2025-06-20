3385cfee96f9d5b312d5b08844f810d88f50b96f
Antonio Rueda-Toicen <antonio.rueda.toicen@gmail.com>
2025-06-20 09:50:44 +0000

    call load_env() on the model.py file

--- app/model.py
+++ app/model.py
@@ -4,8 +4,13 @@ import os
 from torchvision.models import ResNet, resnet18
 from loadotenv import load_env
 
+load_env() # Load environment variables from .env file
+# comment load_env() when doing the GCP deployment      
 wandb_api_key = os.environ.get('WANDB_API_KEY')
+model_path = os.environ.get('MODEL_PATH')
 
 if wandb_api_key:
     wandb.login(key=wandb_api_key)
-    print("Logged in to Weights & Biases")
+    #print("Logged in to Weights & Biases")
+
+print(model_path)
